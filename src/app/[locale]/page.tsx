5e2db264349ea8eb878375b7c1d3358dde30a2bb
Jonathan Kaufmann <jonathan.kaufmann@gmx.net>
2025-07-30 11:07:27 +0200

    feat: remove SignIn component and replace with login link on HomePage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,29 +1,12 @@
 import { useTranslations } from "next-intl";
-import { signIn } from "~/server/auth";
-
-// Since this component doesn't use any interactive features
-// from React, it can be run as a Server Component.
+import { Link } from "~/i18n/navigation";
 
 export default function HomePage() {
   const t = useTranslations("HomePage");
   return (
     <>
       <h1>{t("title")}</h1>
-      <SignIn />
+      <Link href={"auth/login"}>Login</Link>
     </>
-  );
-}
-
-export function SignIn() {
-  return (
-    <form
-      action={async (formData) => {
-        "use server";
-        await signIn("resend", formData);
-      }}
-    >
-      <input type="text" name="email" placeholder="Email" />
-      <button type="submit">Signin with Resend</button>
-    </form>
   );
 }
